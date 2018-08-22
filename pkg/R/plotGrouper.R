c6380690b8ded722c3b8e49fa14214026b003d5b
jdgagnon <jgagnon@Johns-MBP.ucsf.edu>
2018-08-22 15:16:55 -0700

    cleanup

--- pkg/R/plotGrouper.R
+++ pkg/R/plotGrouper.R
@@ -2,16 +2,11 @@
 #' A function to run the shiny app
 #'
 #' This function allows you to run the app
-#' @param ... Takes any arguments that can be called with shiny::runApp()
-#' @keywords plotGrouper
 #' @export
-#' @examples
-#' plotGrouper()
-
 plotGrouper <- function(...)
 {
   # devtools::load_all()
-  appDir <- base::system.file("application", package = "plotGrouper")
+  appDir <- system.file("application", package = "plotGrouper")
   if (appDir == "") {
     stop("Could not find directory. Try re-installing `plotGrouper`.", call. = FALSE)
   }
