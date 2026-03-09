35242d654d00338e3df68202eaf79f75df846980
John Gagnon <john.gagnon.42@gmail.com>
2026-03-09 14:05:26 -0400

    Fix namespace for testthat package

--- run_tests.R
+++ run_tests.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(shinytest)
 
-testthis::test_that("Application works", {
+testthat::test_that("Application works", {
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
   # which runs on Linux.
